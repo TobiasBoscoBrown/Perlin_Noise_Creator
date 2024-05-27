0a2a0f17465d3056cb0ba9c28bd4c3eaf79f1153
Tobiasboscob@gmail.com <tobiasboscob@gmail.com>
2024-05-27 03:12:55 -0600

    Created a new build system using Visual Studio Code. Added in detailed comments to the entire project.

--- perlinNoise1.1/Graph.h
+++ perlinNoise1.1/Graph.h
@@ -6,50 +6,60 @@
 #include <ctime>
 
 using namespace std;
-class Graph
-{
+
+// Define the Graph class
+class Graph {
 private:
-	typedef struct pixel {
-		int red, green, blue;
-		float distanceVectorTR[2], distanceVectorTL[2], distanceVectorBR[2], distanceVectorBL[2];
-		float dotProd1, dotProd2, dotProd3, dotProd4;
-		float inter1, inter2, value;
-	}*pixelPtr;
+    // Define a struct for pixel data
+    typedef struct pixel {
+        int red, green, blue;               // RGB color values
+        float distanceVectorTR[2],          // Distance vectors for different corners
+              distanceVectorTL[2], 
+              distanceVectorBR[2], 
+              distanceVectorBL[2];
+        float dotProd1, dotProd2, dotProd3, dotProd4; // Dot products for interpolation
+        float inter1, inter2, value;       // Intermediate values and final value for the pixel
+    } *pixelPtr; // Pointer to a pixel struct
 
-	typedef struct node {
-		int xPos, yPos;
-		int gradientVector[2];
-		bool isCorner;
-		node* left;
-		node* right;
-		node* up;
-		node* down;
-		pixel* toPixel;
-	}*nodePtr;
+    // Define a struct for node data
+    typedef struct node {
+        int xPos, yPos;                    // Position of the node
+        int gradientVector[2];             // Gradient vector for the node
+        bool isCorner;                     // Flag to indicate if the node is a corner
+        node* left;                        // Pointer to the left neighbor
+        node* right;                       // Pointer to the right neighbor
+        node* up;                          // Pointer to the upper neighbor
+        node* down;                        // Pointer to the lower neighbor
+        pixel* toPixel;                    // Pointer to the corresponding pixel
+    } *nodePtr; // Pointer to a node struct
 
-	nodePtr curr = NULL;
-	nodePtr temp = NULL;
-	nodePtr root = NULL;
-	nodePtr tail = NULL;
-	nodePtr newLineNode = root;
-	nodePtr currAboveNode;
-	nodePtr currCorner;
-	int width, height, cut;
+    nodePtr curr = NULL;         // Current node pointer
+    nodePtr temp = NULL;         // Temporary node pointer
+    nodePtr root = NULL;         // Root node pointer
+    nodePtr tail = NULL;         // Tail node pointer
+    nodePtr newLineNode = root;  // New line node pointer
+    nodePtr currAboveNode;       // Current node above the current one
+    nodePtr currCorner;          // Current corner node pointer
+    int width, height, cut;      // Width, height, and cut properties of the graph
+
 public:
-	void setWidth(int w);
-	void setHeight(int h);
-	void setCut(int c);
+    // Setters
+    void setWidth(int w);        // Set the width of the graph
+    void setHeight(int h);       // Set the height of the graph
+    void setCut(int c);          // Set the cut property of the graph
 
-	int getWidth();
-	int getCut();
-	int getHeight();
-	int simpInt(int i);
-	
-	void addNode(int x, int y);
-	void cutGraph();
-	void defineDistVect();
-	void printNodes();
-	void printPixels();
-	void printCorners();
-	void outputGraph(ofstream&);
+    // Getters
+    int getWidth();              // Get the width of the graph
+    int getCut();                // Get the cut property of the graph
+    int getHeight();             // Get the height of the graph
+    int simpInt(int i);          // Simplify an integer value (implementation not provided)
+
+    // Functions to manipulate the graph
+    void addNode(int x, int y);  // Add a node at position (x, y)
+    void cutGraph();             // Cut the graph
+    void defineDistVect();       // Define the distance vectors for nodes
+    void printNodes();           // Print all nodes in the graph
+    void printPixels();          // Print all pixels in the graph
+    void printCorners();         // Print all corner nodes in the graph
+    void outputGraph(ofstream&); // Output the graph to a file
 };
--- perlinNoise1.1/Source.cpp
+++ perlinNoise1.1/Source.cpp
@@ -1,59 +1,51 @@
-#include <fstream>
-#include <cstdlib>
-#include <time.h>
-#include <iostream>
-#include "Graph.h"
+#include <fstream>    // For file operations (ofstream)
+#include <cstdlib>    // For general purpose functions (rand, srand, abs)
+#include <time.h>     // For time-related functions (time)
+#include <iostream>   // For standard input and output streams (cout, endl)
+#include "Graph.h"    // Custom header file for the Graph class
+#include <cmath>      // For mathematical functions (sin, abs, pow)
+using namespace std;  // Use the standard namespace
 
-using namespace std;
-
-
-int noise(float data, int range);
 int main() {
-		
-	int width = 1001, height = 1001; // this is the length and width of nodes, there will be length-1 * width -1 pixels
-	int numCuts = 100;
+    // Initialize the width and height of the graph (grid) to 1001 x 1001 nodes
+    int width = 1001, height = 1001; // This results in (width-1) * (height-1) pixels
+    int numCuts = 100; // Number of cuts to be made in the graph
 
-	srand(time(0));
+    // Seed the random number generator with the current time
+    srand(time(0));
 
-	Graph pic;
-	pic.setWidth(width);
-	pic.setHeight(height);
-	pic.setCut(numCuts);
+    // Create an instance of the Graph class
+    Graph pic;
+    pic.setWidth(width);   // Set the width of the graph
+    pic.setHeight(height); // Set the height of the graph
+    pic.setCut(numCuts);   // Set the number of cuts in the graph
 
-	ofstream img ("picture.ppm");
-	img << "P3" << endl;
-	img << width - 1 << " " << height - 1<< endl;
-	img << "255" << endl;
+    // Open a file to write the image data in PPM format
+    ofstream img("picture.ppm");
+    img << "P3" << endl;                   // PPM file format identifier
+    img << width - 1 << " " << height - 1 << endl; // Image dimensions (width-1) x (height-1)
+    img << "255" << endl;                  // Maximum color value
 
+    // Add nodes to the graph for each coordinate (x, y)
+    for (int y = 0; y < height; y++) {
+        for (int x = 0; x < width; x++) {
+            pic.addNode(x, y); // Add a node at coordinates (x, y)
+        }
+    }
+	pic.printCorners();
 
-	for (int y = 0; y < height; y++) {
-		for (int x = 0; x < width; x++) {
+    // Perform the specified number of cuts in the graph
+    pic.cutGraph();
 
-			pic.addNode(x, y);
-		}
-	}
-	pic.cutGraph();
-	//pic.printNodes();
-	cout << endl;
-	//pic.printPixels();
-	cout << endl;
-	//pic.printCorners();
-	cout << endl;
-	pic.defineDistVect();
-	pic.outputGraph(img);
+    // Define distance vectors for the nodes in the graph
+    pic.defineDistVect();
 
-	return 0;
-}
+    // Output the graph data to the PPM file
+    pic.outputGraph(img);
 
-int noise(float data, int range) {
-	int noiseSeed = time(0) % 100;
-	
+    // Close the PPM file
+    img.close();
 
-
-	/*data = data + noiseSeed;
-	data = abs(sin(data));
-	data = data * range;*/
-
-	int a = data;
-	return a;
+    // Program finished successfully
+    return 0;
 }
